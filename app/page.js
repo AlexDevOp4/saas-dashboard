6a0a11e19413149123e9dcd7e28946631d2f76cb
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-01-26 14:03:21 -0500

    fixed migration file

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import Login from "./pages/login";
+import Link from "next/link";
+import Layout from "./components/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout/>
+    </>
+  );
+}
